e2fd192043a533aa5519ea3de3ee8208e94f5a69
Kenneth Gerrits <kennethgerrits@gmail.com>
2020-03-17 16:37:56 +0100

    hides manage-users option if no access is granted

--- StudyMate/app/Providers/AuthServiceProvider.php
+++ StudyMate/app/Providers/AuthServiceProvider.php
@@ -25,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        Gate::define('manage-users', function($user){
+            return $user->hasRole('admin');
+        });
+
         Gate::define('edit-users', function($user){
            return $user->hasRole('admin');
         });
--- StudyMate/routes/web.php
+++ StudyMate/routes/web.php
@@ -22,6 +22,6 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::namespace('Admin')->prefix('admin')->name('admin.')->group(function(){
+Route::namespace('Admin')->prefix('admin')->name('admin.')->middleware('can:manage-users')->group(function(){
     Route::resource('/users', 'UsersController', ['except' => ['show', 'create', 'store']]);
 });
