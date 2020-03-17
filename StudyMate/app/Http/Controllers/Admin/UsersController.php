4bd8ff18d448931a93c4ecb544b985e56aa0463e
Kenneth Gerrits <kennethgerrits@gmail.com>
2020-03-17 15:43:20 +0100

    update for user roles implemented

--- StudyMate/app/Http/Controllers/Admin/UsersController.php
+++ StudyMate/app/Http/Controllers/Admin/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use App\Role;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -32,7 +33,12 @@ class UsersController extends Controller
      */
     public function edit(User $user)
     {
-        //
+        $roles = Role::all();
+
+        return view('admin.users.edit')->with([
+            'user' => $user,
+            'roles' => $roles
+        ]);
     }
 
     /**
@@ -44,7 +50,9 @@ class UsersController extends Controller
      */
     public function update(Request $request, User $user)
     {
-        //
+        $user->roles()->sync($request->roles);
+
+        return redirect()->route('admin.users.index');
     }
 
     /**
--- StudyMate/resources/views/admin/users/index.blade.php
+++ StudyMate/resources/views/admin/users/index.blade.php
@@ -8,29 +8,35 @@
                     <div class="card-header">Users</div>
 
                     <div class="card-body">
-                            <table class="table">
-                                <thead>
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Roles</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($users as $user)
                                 <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Name</th>
-                                    <th scope="col">Email</th>
-                                    <th scope="col">Actions</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($users as $user)
-                                    <tr>
-                                        <th scope="row">{{$user->id}}</th>
-                                        <td>{{$user->name}}</td>
-                                        <td>{{$user->email}}</td>
-                                        <td>
+                                    <th scope="row">{{$user->id}}</th>
+                                    <td>{{$user->name}}</td>
+                                    <td>{{$user->email}}</td>
+                                    <td>{{implode(', ', $user->roles()->get()->pluck('name')->toArray())}}</td>
+                                    <td>
+                                        <a href="{{route('admin.users.edit', $user->id)}}">
                                             <button type="button" class="btn btn-primary">Edit</button>
+                                        </a>
+                                        <a href="{{route('admin.users.destroy', $user->id)}}">
                                             <button type="button" class="btn btn-danger">Delete</button>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
-                            </table>
+                                        </a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
 
                     </div>
                 </div>
