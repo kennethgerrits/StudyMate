8aff33f4707eb0bb7179d47ad2ee3d540f406946
Kenneth Gerrits <kennethgerrits@gmail.com>
2020-03-17 15:18:48 +0100

    visual upgrade admin.users.index

--- StudyMate/resources/views/admin/users/index.blade.php
+++ StudyMate/resources/views/admin/users/index.blade.php
@@ -8,9 +8,30 @@
                     <div class="card-header">Users</div>
 
                     <div class="card-body">
-                        @foreach($users as $user)
-                            {{$user->name}} - {{$user->email}}
-                            @endforeach
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                @foreach($users as $user)
+                                    <tr>
+                                        <th scope="row">{{$user->id}}</th>
+                                        <td>{{$user->name}}</td>
+                                        <td>{{$user->email}}</td>
+                                        <td>
+                                            <button type="button" class="btn btn-primary">Edit</button>
+                                            <button type="button" class="btn btn-danger">Delete</button>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+
                     </div>
                 </div>
             </div>
